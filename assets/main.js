9a9dcf5757c73c42bbd304b65477132d9c988205
Sabrinashh <110469877+Sabrinashh@users.noreply.github.com>
2022-10-17 22:59:20 +0400

    file

---
+++ assets/main.js
@@ -0,0 +1,29 @@
+let i1=document.querySelector(".i-1");
+btn.addEventListener("click",function(){
+    let i1=document.querySelector(".i-1");
+    let imgSrc1 = img.src;
+    let img1=document.getElementById("bgimg");
+    this.src=imgSrc1;
+});
+let i2=document.querySelector(".i-2");
+btn.addEventListener("click",function(){
+    let i2=document.querySelector(".i-2");
+    let imgSrc2= img.src;
+    let img2=document.getElementById("bgimg");
+    this.src=imgSrc2;
+});
+let i3=document.querySelector(".i-3");
+btn.addEventListener("click",function(){
+    let i3=document.querySelector(".i-3");
+    let imgSrc3 = img.src;
+    let img3=document.getElementById("bgimg");
+    this.src=imgSrc3;
+});
+let i4=document.querySelector(".i-4");
+btn.addEventListener("click",function(){
+    let i4=document.querySelector(".i-4");
+    let imgSrc4 = img.src;
+    let img4=document.getElementById("bgimg");
+    this.src=imgSrc4;
+});
+
